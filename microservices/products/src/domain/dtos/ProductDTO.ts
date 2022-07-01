1e3b06d73e4fbc2b5cda0f2a8798e5a92d154795
luCAOrx <lucaorx@protonmail.com>
2022-07-01 04:39:18 -0300

    chore: Create a contract of data to requests

---
+++ microservices/products/src/domain/dtos/ProductDTO.ts
@@ -0,0 +1,9 @@
+export interface CreateProductRequestDTO {
+  thumbnail: string;
+  name: string;
+  price: number;
+  ingredients: string;
+  availability: boolean;
+  volume: string;
+  others?: string | null;
+}
