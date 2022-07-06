75dd61f53238ac9e8a4a12af3d821fb3eb5dc967
luCAOrx <lucaorx@protonmail.com>
2022-07-06 00:54:34 -0300

    refactor: Removed function and expected error message

--- microservices/products/src/domain/useCases/UpdateProductUseCase.spec.ts
+++ microservices/products/src/domain/useCases/UpdateProductUseCase.spec.ts
@@ -22,22 +22,20 @@ inMemoryProductRepository.createProducts(createdProduct)
 
 describe('Update product use case', () => {
   it('should not be able to update a product with wrong id', async () => {
-    async function updateProduct () {
-      await sut.execute({
-        productId: 'ddb9b22d-121a-49b0-a39d-d5885a3d0304',
-        data: {
-          name: 'Benegripe',
-          ingredients: 'vitamina c, dipirona mono hidratada',
-          availability: true,
-          volume: '200mg',
-          price: 1000,
-          thumbnail: 'image.jpg',
-          others: 'O medicamento após deve ser mantido em temperatura ambiente'
-        }
-      })
-    }
+    const updateProduct = await sut.execute({
+      productId: 'ddb9b22d-121a-49b0-a39d-d5885a3d0304',
+      data: {
+        name: 'Benegripe',
+        ingredients: 'vitamina c, dipirona mono hidratada',
+        availability: true,
+        volume: '200mg',
+        price: 1000,
+        thumbnail: 'image.jpg',
+        others: 'O medicamento após deve ser mantido em temperatura ambiente'
+      }
+    })
 
-    expect(updateProduct).rejects.toEqual(new Error('Product does not exists.'))
+    expect(updateProduct.isLeft()).toBeTruthy()
   })
 
   it('should be able to update a product', async () => {
