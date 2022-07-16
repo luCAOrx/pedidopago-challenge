d35c5925d147dfc86cc4c15f36b6d68a77e31e79
luCAOrx <lucaorx@protonmail.com>
2022-07-16 03:58:54 -0300

    feat: Create new feature to clone a product

---
+++ microservices/products/src/infra/handlers/CloneProductHandler.ts
@@ -0,0 +1,18 @@
+import { CloneProductUseCase } from '../../domain/useCases/CloneProductUseCase'
+import { PrismaProductRepository } from '../repositories/prisma/PrismaProductRepository'
+
+type CloneProductRequest = { productId: string }
+
+export class CloneProductHandler {
+  async handler ({ productId }: CloneProductRequest) {
+    const prismaProductRepository = new PrismaProductRepository()
+
+    const cloneProductUseCase = new CloneProductUseCase(
+      prismaProductRepository
+    )
+
+    const response = await cloneProductUseCase.execute({ productId })
+
+    return response
+  }
+}
