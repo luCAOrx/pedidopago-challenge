5e21c0d42204ae2bbf88f24d325931335c516dab
luCAOrx <lucaorx@protonmail.com>
2022-07-16 04:15:43 -0300

    feat: Create new feature to update a product

---
+++ microservices/products/src/infra/handlers/UpdateProductHandler.ts
@@ -0,0 +1,19 @@
+import { PrismaProductRepository } from '../repositories/prisma/PrismaProductRepository'
+import { CreateProductRequestDTO } from '../../domain/dtos/ProductDTO'
+import { UpdateProductUseCase } from '../../domain/useCases/UpdateProductUseCase'
+
+type UpdateProductRequest = { productId: string, data: CreateProductRequestDTO }
+
+export class UpdateProductHandler {
+  async handler ({ productId, data }: UpdateProductRequest) {
+    const prismaProductRepository = new PrismaProductRepository()
+
+    const updateProductUseCase = new UpdateProductUseCase(
+      prismaProductRepository
+    )
+
+    const response = updateProductUseCase.execute({ productId, data })
+
+    return response
+  }
+}
