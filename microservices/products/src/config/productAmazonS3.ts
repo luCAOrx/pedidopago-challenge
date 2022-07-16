010923f5a3d5fe08e82caf7a2781ce3e24e52469
luCAOrx <lucaorx@protonmail.com>
2022-07-16 05:12:58 -0300

    refactor: Removed AmazonS3 settings

--- microservices/products/src/config/productAmazonS3.ts
+++
@@ -1,7 +0,0 @@
-export const productAmazonS3Config = {
-  credentials: {
-    accessKeyId: String(process.env.AWS_PRODUCTS_ACCESS_KEY_ID),
-    secretAccessKey: String(process.env.AWS_PRODUCTS_SECRET_ACCESS_KEY)
-  },
-  region: process.env.AWS_PRODUCTS_DEFAULT_REGION
-}
