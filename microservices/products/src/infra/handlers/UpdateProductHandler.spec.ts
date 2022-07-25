0de370cc6e9b82cddb8bd688cd567c7968f6e96a
luCAOrx <lucaorx@protonmail.com>
2022-07-25 18:58:01 -0300

    test: Changed method name and abstracted product creation

--- microservices/products/src/infra/handlers/UpdateProductHandler.spec.ts
+++ microservices/products/src/infra/handlers/UpdateProductHandler.spec.ts
@@ -1,15 +1,37 @@
 import { prisma } from '../prisma/client'
 import { UpdateProductHandler } from './UpdateProductHandler'
+import { randomUUID } from 'node:crypto'
 
 const sut = new UpdateProductHandler()
 
+const id = randomUUID()
+
+const createdProduct = prisma.products.create({
+  data: {
+    id,
+    name: 'Creatina',
+    ingredients: 'Aminoácidos essências',
+    availability: true,
+    volume: '300mg',
+    price: 30,
+    thumbnail: 'image.jpg',
+    others: 'Após deve ser mantido em temperatura ambiente'
+  }
+})
+
 describe('Update product handler', () => {
+  beforeAll(async () => {
+    const createProducts = async () => await createdProduct
+
+    createProducts()
+  })
+
   afterAll(async () => {
     await prisma.products.deleteMany()
   })
 
   it('should not be able to update a product with wrong id', async () => {
-    const response = await sut.handler({
+    const response = await sut.handle({
       productId: 'ddb9b22d-121a-49b0-a39d-d5885a3d0304',
       data: {
         name: 'Dipirona',
@@ -26,20 +48,8 @@ describe('Update product handler', () => {
   })
 
   it('should not be able to update a product with wrong data', async () => {
-    const createdProduct = await prisma.products.create({
-      data: {
-        name: 'Creatina',
-        ingredients: 'Aminoácidos essências',
-        availability: true,
-        volume: '300mg',
-        price: 30,
-        thumbnail: 'image.jpg',
-        others: 'Após deve ser mantido em temperatura ambiente'
-      }
-    })
-
-    const response = await sut.handler({
-      productId: createdProduct.id,
+    const response = await sut.handle({
+      productId: (await createdProduct).id,
       data: {
         name: '',
         ingredients: '',
@@ -55,20 +65,8 @@ describe('Update product handler', () => {
   })
 
   it('should be able to update a product', async () => {
-    const createdProduct = await prisma.products.create({
-      data: {
-        name: 'Creatina',
-        ingredients: 'Aminoácidos essências',
-        availability: true,
-        volume: '300mg',
-        price: 30,
-        thumbnail: 'image.jpg',
-        others: 'Após deve ser mantido em temperatura ambiente'
-      }
-    })
-
-    const response = await sut.handler({
-      productId: createdProduct.id,
+    const response = await sut.handle({
+      productId: (await createdProduct).id,
       data: {
         name: 'Dipirona',
         ingredients: 'dipirona mono-hidratada',
