ea00a25b3795a45c681dd476025c81e5980b1dd9
luCAOrx <lucaorx@protonmail.com>
2022-07-25 18:58:22 -0300

    refactor: Changed method name

--- microservices/products/src/infra/handlers/UpdateProductHandler.ts
+++ microservices/products/src/infra/handlers/UpdateProductHandler.ts
@@ -5,7 +5,7 @@ import { UpdateProductUseCase } from '../../domain/useCases/UpdateProductUseCase
 type UpdateProductRequest = { productId: string, data: CreateProductRequestDTO }
 
 export class UpdateProductHandler {
-  async handler ({ productId, data }: UpdateProductRequest) {
+  async handle ({ productId, data }: UpdateProductRequest) {
     const prismaProductRepository = new PrismaProductRepository()
 
     const updateProductUseCase = new UpdateProductUseCase(
