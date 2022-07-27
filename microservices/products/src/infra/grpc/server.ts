155e64aa4c3716a438be8be37e0e3ae58fe97db6
luCAOrx <lucaorx@protonmail.com>
2022-07-26 21:44:39 -0300

    chore: Added what is needed to run the server

---
+++ microservices/products/src/infra/grpc/server.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+
+import { Server, ServerCredentials } from '@grpc/grpc-js'
+
+import { ProductServer } from './implementations/ProductServer'
+
+import { ProductServiceService } from './proto/product_grpc_pb'
+
+import { promisify } from 'util'
+
+const server = new Server()
+
+const bindPromise = promisify(server.bindAsync).bind(server)
+
+server.addService(ProductServiceService, new ProductServer())
+
+bindPromise(
+  `${process.env.GRPC_PRODUCT_SERVER_URL}`,
+  ServerCredentials.createInsecure()
+).then(() => {
+  server.start()
+  console.log(`listening on ${process.env.GRPC_PRODUCT_SERVER_PORT}`)
+}).catch((error) => console.log(error))
