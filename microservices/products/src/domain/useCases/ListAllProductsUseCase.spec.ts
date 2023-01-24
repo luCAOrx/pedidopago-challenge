61d35740b6c7bc64e2fc880976c98ada4b0480a6
luCAOrx <lucaorx@protonmail.com>
2023-01-24 04:43:06 -0300

    chore: Add make product factory and add expect error for product does not exists

--- microservices/products/src/domain/useCases/ListAllProductsUseCase.spec.ts
+++ microservices/products/src/domain/useCases/ListAllProductsUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { MakeProduct } from '../../../test/factories/MakeProductFactory'
 import { Product } from '../entities/Product'
 import { InMemoryProductRepository } from '../repositories/inMemory/InMemoryProductRepository'
 import { ListAllProductsUseCase } from './ListAllProductsUseCase'
@@ -9,22 +10,14 @@ const sut = new ListAllProductsUseCase(inMemoryProductRepository)
 describe('List all products use case', () => {
   beforeAll(async () => {
     for (let i = 0; i < 20; i++) {
-      const products = Product.create({
-        name: 'Albumina',
-        ingredients: 'clara de ovos',
-        availability: true,
-        volume: '500mg',
-        price: 3000,
-        thumbnail: 'image.jpg',
-        others: 'Após deve ser mantido em temperatura ambiente'
-      }).value as Product
+      const product = (await new MakeProduct().toDomain()).value as Product
 
-      await inMemoryProductRepository.createProducts(products)
+      inMemoryProductRepository.createProducts(product)
     }
   })
 
   it('should be able to list all products', async () => {
-    const product = await sut.execute({ })
+    const product = await sut.execute({})
 
     expect(product.length).toEqual(5)
     expect(product[0].props.name).toEqual('Albumina')
