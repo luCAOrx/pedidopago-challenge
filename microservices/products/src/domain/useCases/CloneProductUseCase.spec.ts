3f1354fc71e61b9af193c518556d0b4bb5efa426
luCAOrx <lucaorx@protonmail.com>
2023-01-24 04:40:36 -0300

    chore: Add make product factory and add expect error for product does not exists

--- microservices/products/src/domain/useCases/CloneProductUseCase.spec.ts
+++ microservices/products/src/domain/useCases/CloneProductUseCase.spec.ts
@@ -1,6 +1,8 @@
+import { MakeProduct } from '../../../test/factories/MakeProductFactory'
 import { Product } from '../entities/Product'
 import { InMemoryProductRepository } from '../repositories/inMemory/InMemoryProductRepository'
 import { CloneProductUseCase } from './CloneProductUseCase'
+import { ProductDoesNotExistsError } from './errors/ProductDoesNotExistsError'
 
 const inMemoryProductRepository = new InMemoryProductRepository()
 
@@ -8,28 +10,21 @@ const sut = new CloneProductUseCase(inMemoryProductRepository)
 
 describe('Clone product use case', () => {
   it('should be able to clone a new product', async () => {
-    const product = Product.create({
-      name: 'Multivitamínico',
-      ingredients: 'vitamina c, dipirona mono hidratada',
-      availability: true,
-      volume: '200mg',
-      price: 1000,
-      thumbnail: 'image.jpg',
-      others: 'O medicamento após deve ser mantido em temperatura ambiente'
-    }).value as Product
+    const product = (await new MakeProduct().toDomain()).value as Product
 
-    await inMemoryProductRepository.createProducts(product)
+    inMemoryProductRepository.createProducts(product)
 
-    const productCloned = await sut.execute({ productId: product.id })
+    const response = await sut.execute({ productId: product.id })
 
-    expect(productCloned).toBeTruthy()
+    expect(response.isRight()).toBeTruthy()
   })
 
   it('should not be able to clone a product', async () => {
-    const cloneProduct = await sut.execute({
+    const response = await sut.execute({
       productId: 'aad54b32-b870-4f93-b0e9-331eb1d1f311'
     })
 
-    expect(cloneProduct.isLeft()).toBeTruthy()
+    expect(response.isLeft()).toBeTruthy()
+    expect(response.value).toBeInstanceOf(ProductDoesNotExistsError)
   })
 })
