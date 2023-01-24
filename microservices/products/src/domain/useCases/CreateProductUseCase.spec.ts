8431c69d58509bc54930b41a13e624ab866c4f1c
luCAOrx <lucaorx@protonmail.com>
2023-01-24 04:41:11 -0300

    chore: Add make product factory

--- microservices/products/src/domain/useCases/CreateProductUseCase.spec.ts
+++ microservices/products/src/domain/useCases/CreateProductUseCase.spec.ts
@@ -1,42 +1,14 @@
-import { CreateProductUseCase } from './CreateProductUseCase'
-
-const createProductSpy = jest.fn()
-const findProductByIdSpy = jest.fn()
-const cloneProductSpy = jest.fn()
-const getProductInformationSpy = jest.fn()
-const listAllProductsSpy = jest.fn()
-const updateProductSpy = jest.fn()
-const deleteProductSpy = jest.fn()
-
-const sut = new CreateProductUseCase({
-  createProducts: createProductSpy,
-  findProductById: findProductByIdSpy,
-  cloneProduct: cloneProductSpy,
-  getProductInformation: getProductInformationSpy,
-  listAllProducts: listAllProductsSpy,
-  updateProduct: updateProductSpy,
-  deleteProduct: deleteProductSpy
-})
+import { MakeProduct } from '../../../test/factories/MakeProductFactory'
 
 describe('Create product use case', () => {
   it('should be able to create a new product', async () => {
-    const response = await sut.execute({
-      name: 'Benegripe',
-      ingredients: 'vitamina c, dipirona mono hidratada',
-      availability: true,
-      volume: '200mg',
-      price: 1000,
-      thumbnail: 'image.jpg',
-      others: 'O medicamento após deve ser mantido em temperatura ambiente'
-    })
+    const product = await new MakeProduct().toDomain()
 
-    expect(response).toBeTruthy()
-
-    expect(createProductSpy).toHaveBeenCalled()
+    expect(product.isRight()).toBeTruthy()
   })
 
   it('should not be able to create a new product', async () => {
-    const response = await sut.execute({
+    const response = await new MakeProduct().toDomain({
       name: '',
       ingredients: '',
       availability: Boolean(),
@@ -47,7 +19,5 @@ describe('Create product use case', () => {
     })
 
     expect(response.isRight()).not.toBeTruthy()
-
-    expect(createProductSpy).not.toHaveBeenCalled()
   })
 })
