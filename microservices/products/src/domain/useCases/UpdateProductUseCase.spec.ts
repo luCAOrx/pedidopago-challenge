b4cbbcd1cf19fe599be7d96694fd7918cb5c495f
luCAOrx <lucaorx@protonmail.com>
2023-01-24 04:43:36 -0300

    chore: Add make product factory and add expect error for product does not exists

--- microservices/products/src/domain/useCases/UpdateProductUseCase.spec.ts
+++ microservices/products/src/domain/useCases/UpdateProductUseCase.spec.ts
@@ -1,63 +1,46 @@
+import { MakeProduct } from '../../../test/factories/MakeProductFactory'
 import { Product } from '../entities/Product'
-import {
-  InMemoryProductRepository
-} from '../repositories/inMemory/InMemoryProductRepository'
+import { InMemoryProductRepository } from '../repositories/inMemory/InMemoryProductRepository'
+import { ProductDoesNotExistsError } from './errors/ProductDoesNotExistsError'
 import { UpdateProductUseCase } from './UpdateProductUseCase'
 
 const inMemoryProductRepository = new InMemoryProductRepository()
 
 const sut = new UpdateProductUseCase(inMemoryProductRepository)
 
-const createdProduct = Product.create({
-  name: 'Albumina',
-  ingredients: 'clara de ovos',
-  availability: true,
-  volume: '500mg',
-  price: 3000,
-  thumbnail: 'image.jpg',
-  others: 'Após deve ser mantido em temperatura ambiente'
-}).value as Product
-
-inMemoryProductRepository.createProducts(createdProduct)
-
 describe('Update product use case', () => {
   it('should not be able to update a product with wrong id', async () => {
-    const updateProduct = await sut.execute({
+    const product = (await new MakeProduct().toDomain()).value as Product
+
+    const response = await sut.execute({
       productId: 'ddb9b22d-121a-49b0-a39d-d5885a3d0304',
-      data: {
-        name: 'Benegripe',
-        ingredients: 'vitamina c, dipirona mono hidratada',
-        availability: true,
-        volume: '200mg',
-        price: 1000,
-        thumbnail: 'image.jpg',
-        others: 'O medicamento após deve ser mantido em temperatura ambiente'
-      }
+      data: product.props
     })
 
-    expect(updateProduct.isLeft()).toBeTruthy()
+    expect(response.isLeft()).toBeTruthy()
+    expect(response.value).toBeInstanceOf(ProductDoesNotExistsError)
   })
 
   it('should be able to update a product', async () => {
+    const product = (await new MakeProduct().toDomain()).value as Product
+
+    inMemoryProductRepository.createProducts(product)
+
     const response = await sut.execute({
-      productId: createdProduct.id,
-      data: {
-        name: 'Benegripe',
-        ingredients: 'vitamina c, dipirona mono hidratada',
-        availability: true,
-        volume: '200mg',
-        price: 1000,
-        thumbnail: 'image.jpg',
-        others: 'O medicamento após deve ser mantido em temperatura ambiente'
-      }
+      productId: product.id,
+      data: product.props
     })
 
-    expect(response).toBeTruthy()
+    expect(response.isRight()).toBeTruthy()
   })
 
   it('should not be able to update a product with wrong data', async () => {
+    const product = (await new MakeProduct().toDomain()).value as Product
+
+    inMemoryProductRepository.createProducts(product)
+
     const response = await sut.execute({
-      productId: createdProduct.id,
+      productId: product.id,
       data: {
         name: '',
         ingredients: '',
@@ -69,6 +52,6 @@ describe('Update product use case', () => {
       }
     })
 
-    expect(response.isRight()).not.toBeTruthy()
+    expect(response.isLeft()).toBeTruthy()
   })
 })
