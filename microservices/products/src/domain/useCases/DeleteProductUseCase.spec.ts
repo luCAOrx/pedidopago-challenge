092aa2ac84aaff06af6af798fad9febfcc8cc74c
luCAOrx <lucaorx@protonmail.com>
2023-01-24 04:41:38 -0300

    chore: Add make product factory and add expect error for product does not exists

--- microservices/products/src/domain/useCases/DeleteProductUseCase.spec.ts
+++ microservices/products/src/domain/useCases/DeleteProductUseCase.spec.ts
@@ -1,36 +1,30 @@
+import { MakeProduct } from '../../../test/factories/MakeProductFactory'
 import { Product } from '../entities/Product'
 import {
   InMemoryProductRepository
 } from '../repositories/inMemory/InMemoryProductRepository'
 import { DeleteProductUseCase } from './DeleteProductUseCase'
+import { ProductDoesNotExistsError } from './errors/ProductDoesNotExistsError'
 
 const inMemoryProductRepository = new InMemoryProductRepository()
 
 const sut = new DeleteProductUseCase(inMemoryProductRepository)
 
-const createdProduct = Product.create({
-  name: 'Albumina',
-  ingredients: 'clara de ovos',
-  availability: true,
-  volume: '500mg',
-  price: 3000,
-  thumbnail: 'image.jpg',
-  others: 'Após deve ser mantido em temperatura ambiente'
-}).value as Product
-
-inMemoryProductRepository.createProducts(createdProduct)
-
 describe('Delete product use case', () => {
   it('should not be able to delete a product with wrong id', async () => {
-    const deleteProduct = await sut.execute({
+    const response = await sut.execute({
       productId: 'ddb9b22d-121a-49b0-a39d-d5885a3d0304'
     })
 
-    expect(deleteProduct.isLeft()).toBeTruthy()
+    expect(response.isLeft()).toBeTruthy()
+    expect(response.value).toBeInstanceOf(ProductDoesNotExistsError)
   })
 
   it('should be able to delete a product', async () => {
-    const response = await sut.execute({ productId: createdProduct.id })
+    const product = (await new MakeProduct().toDomain()).value as Product
+    inMemoryProductRepository.createProducts(product)
+
+    const response = await sut.execute({ productId: product.id })
 
     expect(response.isRight()).toBeTruthy()
     expect(inMemoryProductRepository.items.length).toEqual(0)
